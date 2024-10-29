c2d095efd31df41e2c8bf5332738eb00bfd97c6f
RatLad69 <quirinus613@gmail.com>
2024-10-29 01:25:20 -0500

    solver function implemented

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,72 @@ import (
 
 type Sd struct {
 	Vals []int `json:"sdVals"`
+	Size int   `json:"size"`
+}
+
+func splitSd(sd Sd) [][]int {
+	numRows := sd.Size
+	row := make([]int, numRows, numRows)
+	valSquare := make([][]int, numRows, numRows)
+	for i := 0; i < numRows; i++ {
+		row = sd.Vals[numRows*i : numRows*i+numRows]
+		valSquare[i] = row
+	}
+
+	return valSquare
+}
+
+func isLegalPlacement(sd [][]int, col int, row int, val int) bool {
+	for i := 0; i < len(sd); i++ {
+		if sd[row][i] == val {
+			return false
+		}
+	}
+
+	for i := 0; i < len(sd); i++ {
+		if sd[i][col] == val {
+			return false
+		}
+	}
+
+	subSquareRow0 := row - row%3
+	subSquareCol0 := col - col%3
+
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			if sd[subSquareRow0+i][subSquareCol0+j] == val {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func sdSolve(sq *[][]int, size int, row int, col int) bool {
+	if row == size-1 && col == size {
+		return true
+	}
+
+	if col == size {
+		row++
+		col = 0
+	}
+
+	if (*sq)[row][col] > 0 {
+		return sdSolve(sq, size, row, col+1)
+	}
+
+	for val := 1; val <= size; val++ {
+		if isLegalPlacement(*sq, col, row, val) {
+			(*sq)[row][col] = val
+			if sdSolve(sq, size, row, col+1) {
+				return true
+			}
+		}
+		(*sq)[row][col] = 0
+	}
+	return false
 }
 
 func main() {
@@ -32,7 +98,14 @@ func main() {
 			}
 			defer r.Body.Close()
 
-			fmt.Println(sd.Vals)
+			sdSquare := splitSd(sd)
+			fmt.Println(sdSquare)
+			fmt.Println("-------")
+			if sdSolve(&sdSquare, sd.Size, 0, 0) {
+				fmt.Println(sdSquare)
+			} else {
+				fmt.Println("Failed to solve")
+			}
 		}
 
 		msg := map[string]string{"message": "Hello from the solver"}
